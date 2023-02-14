f01641f0fe11f5c826f9c1d30612c76944e38eb0
AnjalBamanu10 <anjalbamanu@gmail.com>
2023-02-14 17:38:43 +0545

    initial commit

---
+++ Resturant.java
@@ -0,0 +1,18 @@
+public class Resturant{
+    public static void  main(String[] args){
+        System.out.println("Name of the Organiztion: Kamalpokhari Resturant & Bar");
+        System.out.println();
+        System.out.println();
+        System.out.println("Adress:");
+        System.out.println("----State: KM");
+        System.out.println("----Zip Code: 44600");
+        System.out.println("----Country: Nepal");
+        System.out.println();
+        System.out.println();
+        System.out.println("Contact Information:");
+        System.out.println("----Mobile Number: 9865382409");
+        System.out.println("----Email: kamlapokhari.rnb@gmail.com");
+
+
+    }
+}
